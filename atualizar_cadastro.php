f487250e326cbea8cbb84e9218476659a53111ee
José H. Coca <jhfcoca@hotmail.com>
2021-10-21 23:27:22 -0300

    noix

---
+++ atualizar_cadastro.php
@@ -0,0 +1,34 @@
+<?php
+    include_once('conexao.php');
+
+    //recuperando os dados do formulário
+    $idFilme = $_POST[$ele];
+    $titulo = $_POST['titulo'];
+    $sinopse = $_POST['sinopse'];
+    $genero = $_POST['genero'];
+    $data_lancamento = $_POST['data_lancamento'];
+    $duracao = $_POST['duracao'];
+
+    //fazendo o insert na tabela
+    $sql_filme = "insert into filme (titulo, sinopse, genero, data_lancamento, duracao) values ('$titulo',
+                    '$sinopse', '$genero', '$data_lancamento', '$duracao');"; 
+
+    //executando o insert na tabela
+    $resultado_filme = mysqli_query($conn, $sql_filme);
+?>
+<!DOCTYPE htlm>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Atualizar Cadastro | FATECINE</title>
+</head>
+<body>
+    <div class="box">
+        <fieldset>
+            <legend><b>CADASTRO</b></legend><br>
+            <h1 align='center'>Cadastro conluído</h1>
+            <a href="index.php"><button class="btnGenerico">Voltar</button></a><br><br>
+        </fieldset>
+    </div>
+</body>
+</html>
--- exibicao_filme.php
+++ exibicao_filme.php
@@ -2,46 +2,42 @@
     include_once('conexao.php');
 
     $nomeFilme = $_POST['listBox'];
+    echo $nomeFilme;
 
     $sql = "select * from filme where titulo='$nomeFilme';";
     $resultado = mysqli_query($conn, $sql);
     $elemento = mysqli_fetch_array($resultado);
-
-    $nomeFilme = $elemento['titulo'];
-    $sinopseFilme = $elemento['sinopse'];
-    $generoFilme = $elemento['genero'];
-    $dataFilme = $elemento['data_lancamento'];
-    $duracaoFilme = $elemento['duracao'];
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Filme | FATECINE</title>
+    <title>Exibição Filme | FATECINE</title>
 </head>
 <body>
     <div class="box">
         <fieldset>
             <legend><b>FILME</b></legend><br>
+            <form action="atualizar_cadastro.php" method="POST">
                 <div class="inputBox">
-                    <input type="text" name="titulo" class="inputUser" value="<?php echo $nomeFilme?>">
+                    <input type="text" name="titulo" class="inputUser" value="<?php echo $elemento['titulo'];?>">
                     <label for="titulo" class="labelInput">Título</label>
                 </div><br><br>
                 <div class="inputBox">
-                    <textarea name="sinopse" class="inputUser"><?php echo $sinopseFilme?></textarea>
+                    <textarea name="sinopse" class="inputUser"><?php echo $elemento['sinopse'];?></textarea>
                     <label for="sinopse" class="labelInput">Sinopse</label>
                 </div><br><br>
                 <div class="inputBox">
-                    <input type="text" name="genero" class="inputUser" value="<?php echo $generoFilme?>">
+                    <input type="text" name="genero" class="inputUser" value="<?php echo $elemento['genero'];?>">
                     <label for="genero" class="labelInput">Gênero</label>
                 </div><br><br>
                 <div class="inputBox">
                     <label for="data_lancamento"><b>Data de lançamento:</b></label>
-                    <input type="text" name="data_lancamento" id="data_lancamento" value="<?php echo $dataFilme ?>">
+                    <input type="text" name="data_lancamento" id="data_lancamento" value="<?php echo $elemento['data_lancamento'];?>">
                 </div><br><br>
                 <div class="inputBox">
-                    <input type="text" name="duracao" class="inputUser" value="<?php echo $duracaoFilme?>">
+                    <input type="text" name="duracao" class="inputUser" value="<?php echo $elemento['duracao'];?>">
                     <label for="duracao" class="labelInput">Duração</label>
                 </div><br><br>
                 <a href="index.php"><input class="btnExibicao" value="Voltar"></a>
